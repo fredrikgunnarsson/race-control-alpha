cfe5c16bbe8dad37e523db4d9e63c5ce5e6b2f28
fredrikgunnarsson <fredrik.s.gunnarsson@gmail.com>
2020-03-20 17:52:51 +0100

    stor omdesign

--- app.js
+++ app.js
@@ -1,24 +1,265 @@
-let app = require('express')();
+let express = require('express');
+let app = express();
 let http = require('http').createServer(app);
 let io = require('socket.io')(http);
 
-app.get('/', (req,res)=>{
-    res.sendFile(__dirname+ '/index.html')
-})
+app.use('/', express.static('public'))
+
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname+ '/index.html')
+// })
+
+const flagsSchema = [
+    {
+        name:'flag-finish', 
+        needNumber:false, 
+        canBlink:false, 
+        prio:2,
+        save:false, 
+        pause:false, 
+        allScreen:false, 
+        isSignal:true,
+    },
+    {
+        name:'flag-yellow-red', 
+        needNumber:false, 
+        canBlink:false, 
+        prio:5, 
+        save:false, 
+        pause:true, 
+        allScreen:false, 
+        isSignal:true,
+    },
+    {
+        name:'flag-green', 
+        needNumber:false, 
+        canBlink:false, 
+        prio:6,
+        save:false, 
+        pause:true, 
+        allScreen:false, 
+        isSignal:true,
+    },
+    {
+        name:'flag-white', 
+        needNumber:false, 
+        canBlink:true, 
+        prio:4,
+        save:false, 
+        pause:true, 
+        allScreen:false, 
+        isSignal:true,
+    },
+    {
+        name:'flag-yellow', 
+        needNumber:false, 
+        canBlink:true, 
+        prio:3, 
+        save:false, 
+        pause:true, 
+        allScreen:false, 
+        isSignal:true,
+    },
+    {
+        name:'flag-red', 
+        needNumber:false, 
+        canBlink:true, 
+        prio:1, 
+        save:false, 
+        pause:false, 
+        allScreen:true, 
+        isSignal:true,
+    },
+    {
+        name:'flag-black', 
+        needNumber:true, 
+        canBlink:false, 
+        prio:10,
+        save:true, 
+        pause:false, 
+        allScreen:false, 
+        isSignal:false,
+    },
+    {
+        name:'flag-black-white', 
+        needNumber:true, 
+        canBlink:false, 
+        prio:10,
+        save:true, 
+        pause:false, 
+        allScreen:false, 
+        isSignal:false,
+    },
+    {
+        name:'flag-black-orange', 
+        needNumber:true, 
+        canBlink:false, 
+        prio:10,
+        save:true, 
+        pause:false, 
+        allScreen:false, 
+        isSignal:false,
+    },
+    {
+        name:'flag-blue', 
+        needNumber:false, 
+        canBlink:false, 
+        prio:10,
+        save:false, 
+        pause:false, 
+        allScreen:false, 
+        isSignal:false,
+    },
+    {
+        name:'flag-sc', 
+        needNumber:false, 
+        canBlink:true, 
+        prio:3,
+        save:false, 
+        pause:true, 
+        allScreen:false, 
+        isSignal:true,
+    },
+    {
+        name:'flag-vsc', 
+        needNumber:false, 
+        canBlink:true, 
+        prio:3,
+        save:false, 
+        pause:true, 
+        allScreen:false, 
+        isSignal:true,
+    },
+]
 
 let PORT = process.env.PORT || 3009;
 let flags = []
 let screens = [];
+let screens2 = [
+    {
+        section:0,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:1,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:2,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:3,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:4,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:5,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:6,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:7,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:8,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:9,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:10,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:11,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+    {
+        section:12,
+        clients:[],
+        flags:[],
+        active:false,
+    },
+];
+let config = {
+    shiftTime:1000,
+    blinkTime:500,
+}
+
+app.get('/api/flags',(req,res,next)=>{
+    res.json(flagsSchema)
+});
+app.get('/flag',(req,res) => {
+    res.sendFile(__dirname + '/public/flag.html')
+})
+app.get('/section/:id',(req,res)=>{
+    res.sendFile(__dirname + '/public/flag.html')
+})
+app.get('/kontroll',(req,res)=>{
+    res.sendFile(__dirname + '/public/kontroll.html')
+})
+app.get('/screens',(req,res)=>{
+    res.header("Content-Type",'application/json');
+    res.send(JSON.stringify(screens2, null, 4));
+})
 
 io.on('connection', socket => {
     console.log(new Date,`server connection initiated... id: ${socket.id}`)
     screens.push({id:socket.id})
     io.emit('checkClientScreen')
+    updateClient();
 
     socket.on('disconnect',()=>{
         console.log(new Date, `server connection disconnected... id: ${socket.id}`)
         let idx = screens.findIndex(el => el.id==socket.id)
         screens.splice(idx,1)
+
+        screens2.forEach(el=>{
+            let idx = el.clients.indexOf(socket.id);
+            if (idx > -1) {
+                el.clients.splice(idx,1);
+                if (el.clients.length==0) {
+                    console.log('last screen gone...')
+                    el.active=false;
+                    el.flags=[]
+                }
+            }
+        })
+
         updateClient()
     })
     socket.on('clientScreenInfo',({id,screen})=>{
@@ -35,6 +276,16 @@ io.on('connection', socket => {
         }
         updateClient();
     })
+    socket.on('selectFlag2',(clickedFlag)=>{
+        let activeScreen = screens2.filter(el=>el.active)[0];
+        if (!activeScreen) return console.log('...no active screen');
+        let doExist = activeScreen.flags.indexOf(clickedFlag);
+        (doExist < 0) 
+            ? activeScreen.flags.push(clickedFlag) 
+            : activeScreen.flags.splice(doExist,1);
+        // console.log(clickedFlag, ` isActive:${isActive}  isOnline:${isOnline}... ${JSON.stringify(clickedSection)}`)
+        updateClient();
+    })
     socket.on('selectNumber',({flag, number})=>{
         console.log(flag, number)
         let idx = flags.findIndex(el => el.flag==flag)
@@ -42,16 +293,41 @@ io.on('connection', socket => {
         updateClient();
     })
 
-    socket.on('selectScreenRole', ({id,role})=>{
-        let idx = screens.findIndex(el => el.id==id);
-        screens[idx].role=role
+    socket.on('sectionUpdate',({section})=>{
+        let idx = screens2.findIndex(el=>el.section==section);
+        if (idx>-1) screens2[idx].clients.push(socket.id);
+    })
+    socket.on('clickSection',({section})=>{
+        let clickedSection = screens2.filter(el=>el.section==section)[0]
+        let activeSections = screens2.filter(el=>el.active);
+        let isActive = clickedSection.active
+        let isOnline = (clickedSection.clients.length > 0) ? true : false;
+
+        if (isOnline) {
+            activeSections.forEach(el => el.active=false)
+            clickedSection.active ^= true
+        }
+        console.log(section, ` isActive:${isActive}  isOnline:${isOnline}... ${JSON.stringify(clickedSection)}`)
         updateClient()
     })
+
+    // socket.on('selectScreenRole', ({id,role})=>{
+    //     console.log(role);
+        
+    //     let idx = screens.findIndex(el => el.id==id);
+    //     screens[idx].role=role
+    //     updateClient()
+    // })
 })
 
 function updateClient() {
-    io.emit('updateClient',{screens,flags})
+    io.emit('updateClient',{screens,flags, screens2})
 }
+
+setInterval(() => {
+    console.log('...');
+    
+}, 1000);
 
 
 http.listen(PORT, ()=>{
---
+++ public/flag.js
@@ -0,0 +1,77 @@
+        // ===============================
+        // INITIALIZATIONS
+        // ===============================
+
+        let socket=io();
+
+        let flagsSchema = [{}];
+
+        (function getFlags() {
+            return fetch('/api/flags')
+            .then(res => res.json())
+            .then(data => {
+                flagsSchema = data;
+                startFlagCarousel();
+            })
+        })()
+       
+        let section = document.location.pathname.match(/\/section\/(\d{1,2})/)[1]
+
+        let clientScreen = {
+            role:'flag',
+            section:section,
+            flags:[],
+        }
+
+        let serverState=[];
+        let flagNumber='';
+        let carouselCounter = 0;
+
+        const screenNameElement = document.querySelector('#screen-name');
+        const flagScreenElement = document.querySelector('#flag-screen');
+        const flagDisplayElement = document.querySelector('.flag-display');
+        const numberDisplayElement = document.querySelector('.number-display');
+
+
+        // ===============================
+        // SOCKETS
+        // ===============================
+
+        
+        socket.on('connect',()=>{
+            clientScreen.id=socket.id;
+            screenNameElement.innerHTML=socket.id;
+            socket.emit('sectionUpdate',({section:clientScreen.section}))
+        })
+        socket.on('disconnect',()=>{
+            screenNameElement.innerHTML='server disconnected!!!';
+        })
+        
+        socket.on('checkClientScreen',()=>{
+            socket.emit('clientScreenInfo',{id:socket.id, screen:clientScreen});
+        })
+        socket.on('updateClient',({screens2})=>{
+            serverState=screens2;
+        })
+
+        
+        // ===============================
+        // HELPER FUNCTIONS
+        // ===============================
+
+
+        function startFlagCarousel() {
+
+            setTimeout(startFlagCarousel, 3000);
+
+            let thisSection = serverState.find(el=>el.section==section);
+            let displayDiv = document.querySelector('.flag-display');
+
+            if(thisSection && thisSection.flags.length < 1) {
+                displayDiv.className="flag-display";
+            } else {
+                displayDiv.className=`flag-display flag ${thisSection.flags[carouselCounter % thisSection.flags.length]}`;
+            }
+
+            carouselCounter++;
+        }
